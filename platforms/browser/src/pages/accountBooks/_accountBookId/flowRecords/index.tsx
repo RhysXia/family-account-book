4546bef2d6da80de4c8fc66ab214293bd83a86de
Rhys Xia <xrs4433@outlook.com>
2022-10-15 21:16:38 +0800

    feat: update

--- platforms/browser/src/pages/accountBooks/_accountBookId/flowRecords/index.tsx
+++ platforms/browser/src/pages/accountBooks/_accountBookId/flowRecords/index.tsx
@@ -82,10 +82,6 @@ const Index = () => {
           field: 'dealAt',
           direction: 'DESC',
         },
-        {
-          field: 'createdAt',
-          direction: 'DESC',
-        },
       ],
     },
   });
