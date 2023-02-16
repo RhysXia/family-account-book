6a5b975d91142b7ec2b18434ae01eedc43978237
Rhys Xia <xrs4433@outlook.com>
2023-02-16 22:47:14 +0800

    feat: 支持pwa

--- platforms/browser/vite.config.ts
+++ platforms/browser/vite.config.ts
@@ -5,6 +5,7 @@ import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh';
 import Pages from 'vite-plugin-pages';
 import { resolve } from 'path';
 import graphqlTag from 'babel-plugin-graphql-tag';
+import { VitePWA } from 'vite-plugin-pwa';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
@@ -26,6 +27,30 @@ export default defineConfig({
     emptyOutDir: true,
   },
   plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      // devOptions: {
+      //   enabled: true,
+      // },
+      manifest: {
+        name: '记账平台',
+        short_name: '记账平台',
+        description: '多人多账本记账平台',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
     react({
       babel: {
         plugins: [
